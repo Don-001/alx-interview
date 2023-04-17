2025786c1824f551cc32c0ac8a80733669dcbb38
Don-001 <claudiodex009@gmail.com>
2023-04-17 14:13:08 +0300

    Solve Pascal's Triangle problem

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def pascals_triangle(n):
+    triangle = [[1]]
+    for i in range(n-1):
+        row = []
+        row.append(triangle[i][0])
+        for j in range(len(triangle[i])-1):
+            row.append(triangle[i][j] + triangle[i][j+1])
+        row.append(triangle[i][-1])
+        triangle.append(row)
+    return triangle
